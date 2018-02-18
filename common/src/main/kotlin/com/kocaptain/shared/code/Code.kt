553aeb684662ce962edd67361c3419cbcd3bf082
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2018-02-18 11:29:28 -0500

    Added more utility functions on Free1.

--- common/src/main/kotlin/com/kocaptain/shared/code/Code.kt
+++ common/src/main/kotlin/com/kocaptain/shared/code/Code.kt
@@ -20,25 +20,12 @@ private const val guidKey = "loggedin_user_guid"
 // Gets user's guid from settings, if not there, download user profile with guid and save.
 
 val getUserGuid: Free1<String?> =
-    getSettingString(guidKey)
-        .bind { nullableGuid ->
-            nullableGuid
-                ?.let(::Lift)
-                ?: Zip(getLoginId, getGpsLocation()) { nulLoginId, location ->
-                    nulLoginId
-                        ?.let { loginId ->
-                            getProfile(loginId, location)
-                                .bind { eitherProfile ->
-                                    when (eitherProfile) {
-                                        is Either.Right -> {
-                                            putSettingString(guidKey, eitherProfile.value.guid)
-                                                .apRight(Lift(eitherProfile.value.guid))
-                                        }
-
-                                        is Either.Left -> Lift(null)
-                                    }
-                                }
-                        }
-                        ?: Lift(null)
-                }
+    getSettingString(guidKey).apNextIfNull(
+        getLoginId.bindIfNotNull { loginId ->
+            getGpsLocation()
+                .bind { nullableLocation -> getProfile(loginId, nullableLocation) }
+                .rightOrNull()
+                .mapIfNotNull { nullableProfile -> nullableProfile.guid }
+                .doIfNotNull { guid -> putSettingString(guidKey, guid) }
         }
+    )
--- common/src/main/kotlin/com/kocaptain/utils/Free.kt
+++ common/src/main/kotlin/com/kocaptain/utils/Free.kt
@@ -35,10 +35,73 @@ expect class FreeOperation<A>(s: Operation<A>) : Free1<A> {
     val s: Operation<A>
 }
 
-// Monad and Applicative functions on Free.
+// Monad and Applicative functions on Free1<A>.
 
 fun <A, B> Free1<A>.map(f: (A) -> B): Free2<A, B> = Map(this, f)
 fun <A> Free1<Free1<A>>.flatten(): Free1<A> = Flatten(this)
 fun <A, B> Free1<A>.bind(f: (A) -> Free1<B>): Free1<B> = map(f).flatten()
-fun <A, B> Free1<A>.apLeft(sb: Free1<B>): Free1<A> = bind { x -> sb.bind { Lift(x) } }
-fun <A, B> Free1<A>.apRight(sb: Free1<B>): Free1<B> = bind { sb }
+fun <A, B> Free1<A>.apPrev(sb: Free1<B>): Free1<A> = bind { x -> sb.bind { Lift(x) } }
+fun <A, B> Free1<A>.apNext(sb: Free1<B>): Free1<B> = bind { sb }
+
+// Utility functions on Free1<A?>
+
+inline fun <A : Any, B : Any> Free1<A?>.bindIfNotNull(crossinline f: (A) -> Free1<B?>): Free1<B?> =
+    bind { x -> x?.let(f) ?: Lift(null) }
+
+inline fun <A : Any, B : Any> Free1<A?>.mapIfNotNull(crossinline f: (A) -> B?): Free1<B?> =
+    map { x -> x?.let(f) }
+
+fun <A : Any> Free1<A?>.apNextIfNull(av: Free1<A?>): Free1<A?> =
+    bind { x -> x?.let(::Lift) ?: av }
+
+fun <A : Any, B> Free1<A?>.doIfNotNull(f: (A) -> Free1<B>): Free1<A?> =
+    bind { x -> x?.run { f(x).apNext(Lift(x)) } ?: Lift(null) }
+
+// Utility functions on Free1<Either<A, B>>
+
+inline fun <A, B, C> Free1<Either<A, B>>.mapRight(crossinline f: (B) -> C): Free1<Either<A, C>> =
+    map { either ->
+        when (either) {
+            is Either.Left -> either
+            is Either.Right -> Either.Right(f(either.value))
+        }
+    }
+
+fun <A, B : Any> Free1<Either<A, B?>>.rightOrNull(): Free1<B?> =
+    map { either ->
+        when (either) {
+            is Either.Left -> null
+            is Either.Right -> either.value
+        }
+    }
+
+fun <A, B : Any, C : Any> Free1<Either<A, B?>>.bindIfRightNotNull(
+    f: (B) -> Either<A, C>
+): Free1<Either<A, C?>> =
+    bind { either ->
+        when (either) {
+            is Either.Left -> either
+            is Either.Right -> either.value?.let(f) ?: Either.Right(null)
+        }
+            .let(::Lift)
+    }
+
+fun <A, B : Any, C : Any> Free1<Either<A, B?>>.mapRightIfNotNull(
+    f: (B) -> C?
+): Free1<Either<A, C?>> =
+    map { either ->
+        when (either) {
+            is Either.Left -> either
+            is Either.Right -> either.value?.let(f).let { Either.Right(it) }
+        }
+    }
+
+fun <A, B : Any> Free1<Either<A, B?>>.apNextIfRightNull(
+    av: Free1<Either<A, B?>>
+): Free1<Either<A, B?>> =
+    bind { either ->
+        when (either) {
+            is Either.Left -> Lift(either)
+            is Either.Right -> either.value?.run { Lift(either) } ?: av
+        }
+    }
